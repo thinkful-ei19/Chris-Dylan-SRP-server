76f8d0bed77274c8eb6ecacaf90048adffc2eaed
Christopher <cjszk@hotmail.com>
2018-05-28 10:30:38 -0700

    fixed auth

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ module.exports = {
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
         process.env.DATABASE_URL || 'mongodb://chris:dylan@ds235860.mlab.com:35860/chris-dylan-srp',
-  TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test',
+//   TEST_DATABASE_URL:
+//         process.env.TEST_DATABASE_URL ||
+//         'mongodb://localhost/thinkful-backend-test',
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
   // TEST_DATABASE_URL:
